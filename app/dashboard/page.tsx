a1de7bdd37a860e413521c07a3827488e8a93f11
KIHARA-Keito <kiharakeito@gmail.com>
2023-11-25 00:21:29 +0900

    add modal test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,46 @@
+"use client";
+
 export default function Page() {
   return (
     <>
       <p className="">Dashboard Page</p>
-      <button class="btn">Button</button>
-      <button class="btn btn-primary">Button</button>
-      <button class="btn btn-accent">Button</button>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-accent">Button</button>
+
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_2").showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click outside to close</p>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+
+      {/* The button to open modal */}
+      <a href="#my_modal_8" className="btn">
+        open modal
+      </a>
+      {/* Put this part before </body> tag */}
+      <div className="modal" role="dialog" id="my_modal_8">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">This modal works with anchor links</p>
+          <div className="modal-action">
+            <a href="#" className="btn">
+              Yay!
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
